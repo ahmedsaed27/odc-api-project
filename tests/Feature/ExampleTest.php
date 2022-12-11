1c0ed1f59ff52a254c723829d27b90b021f74a28
ahmedsaed27 <as9840935@gmail.com>
2022-12-11 12:04:36 +0200

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 class ExampleTest extends TestCase
 {
@@ -17,5 +18,16 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+
+    public function test_login(){
+        $auth = $this->postJson('http://127.0.0.1:8000/api/login' , [
+            'email' => 'user@gmail.com',
+            'password' => 'password',
+            'name' => 'User',
+            'api_password' => 'odcAPOXZASSAFAS@@BCSA'
+        ]);
+        $auth->assertStatus(200)->assertJson(['status' => 'success']);
     }
 }
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\AuthController;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+
+  
+}
